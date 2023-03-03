023dca0f32923333a27180ce17640b07463d74d5
Avazbek <avazbeknematov01@gmail.com>
2023-03-03 15:40:52 +0500

    development

--- src/router/navigators.js
+++ src/router/navigators.js
@@ -1,31 +1,31 @@
 const navigators = [
 
-    {
-        title: 'Регионы',
-        route: 'regions.index',
-        icon: 'mdi-map-marker',
-
-    },
-    {
-        title: 'Районы',
-        icon: 'mdi-city',
-        route: 'districts.index',
-    },
+    // {
+    //     title: 'Регионы',
+    //     route: 'regions.index',
+    //     icon: 'mdi-map-marker',
+    //
+    // },
+    // {
+    //     title: 'Районы',
+    //     icon: 'mdi-city',
+    //     route: 'districts.index',
+    // },
     // {
     //     title: 'МФИ',
     //     icon: 'mdi-home-city',
     //     route: 'mfies.index',
     // },
-    {
-        title: 'Агротехник тадбирлар',
-        icon: 'mdi-home-city',
-        route: 'arrangements.index',
-    },
-    {
-        title: 'Экин турлари',
-        icon: 'mdi-tree',
-        route: 'plant_types.index',
-    },
+    // {
+    //     title: 'Агротехник тадбирлар',
+    //     icon: 'mdi-home-city',
+    //     route: 'arrangements.index',
+    // },
+    // {
+    //     title: 'Экин турлари',
+    //     icon: 'mdi-tree',
+    //     route: 'plant_types.index',
+    // },
     {
         title: "Технологик карта",
         icon: "mdi-tractor",
